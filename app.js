1d901732aa330109feeeb6eebff20c31aaef29dc
Aninda <aninda.r.00@gmail.com>
2023-02-04 12:58:43 +0530

    first commit

--- app.js
+++ app.js
@@ -12,7 +12,14 @@
 //npm init (step by step, press enter to skip)
 //npm init -y (everything default)
 
+//npm install (installs all dependencies from package.json file)
+//npm i nodemon -D (installs the module as a dev dependency)
+//dev depency does not get pushed to production while running npm install
+
+//npm uninstall packageName for uninstalling a module/package/dependency
+
 const _ = require('lodash')
 const items = [1,[2,[3,[4]]]]
 const newItems = _.flattenDeep(items);
 console.log(newItems)
+console.log('Hello world!!')
